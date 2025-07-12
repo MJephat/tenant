403d463f31d5937df042604a37268e438219fa45
MJephat <mukundijephat22@gmail.com>
2025-07-12 12:59:36 +0300

    Fix:API connection

--- client/src/assets/axios.jsx
+++ client/src/assets/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api/v1",
+    baseURL: "https://tenant-kx95.onrender.com/",
     withCredentials: true,
   });
   
