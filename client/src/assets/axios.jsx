ac8dc90a78b6245adc253cb4559a766e338962e1
MJephat <mukundijephat22@gmail.com>
2025-07-12 14:18:42 +0300

    Fix: API baseURL and CORS for production

--- client/src/assets/axios.jsx
+++ client/src/assets/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: "https://tenant-kx95.onrender.com/api/v1",
+    baseURL: "https://tenant-kx95.onrender.com",
     withCredentials: true,
   });
   
