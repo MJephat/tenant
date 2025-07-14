2f41b82ce3eb23fc41d1135791e557b88bffd623
MJephat <mukundijephat22@gmail.com>
2025-07-14 22:27:09 +0300

    removed a backslash

--- client/src/assets/axios.jsx
+++ client/src/assets/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: "https://tenant-aa3z.onrender.com/",
+    baseURL: "https://tenant-aa3z.onrender.com",
     withCredentials: true,
   });
   
