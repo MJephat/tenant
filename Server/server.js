eb09675d7481e0e6d456a1021d59bd5e8e337b94
MJephat <mukundijephat22@gmail.com>
2025-07-14 08:56:27 +0300

    reverse backend

--- Server/server.js
+++ Server/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001
 connectDB();
 
 app.use(cors({
-    origin:[ "http://localhost:5173", 'https://tenant-kx95.onrender.com'],
+    origin: "http://localhost:5173",
     credentials: true,           
   }));
 
