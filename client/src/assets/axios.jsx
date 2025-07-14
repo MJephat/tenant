21c1fd32c68f1cb1226f531046132b36ecdbabbd
MJephat <mukundijephat22@gmail.com>
2025-07-14 22:20:28 +0300

    changed to render url

--- client/src/assets/axios.jsx
+++ client/src/assets/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: "https://tenant-aa3z.onrender.com/api/v1",
+    baseURL: "https://tenant-aa3z.onrender.com/",
     withCredentials: true,
   });
   
